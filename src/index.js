edc9a18a630d0dee4571b286441cd7943326309e
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-09 08:02:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+function fetchImg() {
+  fetch(imgUrl)
+  .then(resp => resp.json())
+  .then( results => {
+   results.message.forEach(image => renderImg(image))
+  })
+} 
+
+function renderImg(imageAr){
+  const picSpot = document.getElementById("dog-image-container")
+  let imgEl = document.createElement('img')
+  imgEl.src = imageAr;
+  picSpot.appendChild(imgEl)
+  };
+    
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded",() => {
+fetchImg();
+
+});
